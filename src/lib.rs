164c4a55bcd9d6ff13aa71e82fca2e5abff4d31f
Tim Neumann <timnn@google.com>
2018-04-05 21:28:00 +0200

    initial implementation of travis::Client

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+#![deny(unused_must_use)]
+
 #[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate hyper;
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+pub static USER_AGENT: &str = concat!("rust-ops/rust-log-analyzer ", env!("CARGO_PKG_VERSION"));
+
+pub mod travis;
 
 pub type Result<T> = std::result::Result<T, failure::Error>;
