47107c54bc073b183620c9c4c75b3666f85343a3
Tim Neumann <timnn@google.com>
2018-04-07 18:43:07 +0200

    implement index + some reorg

---
+++ src/index/mod.rs
@@ -0,0 +1,90 @@
+use super::Result;
+use atomicwrites;
+use bincode;
+use fnv;
+use std::fs;
+use std::path::Path;
+use std::slice;
+
+mod table;
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct Index {
+    internal: fnv::FnvHashMap<u32, u32>,
+}
+
+impl Index {
+    pub fn learn(&mut self, data: &[u8], multiplier: u32) {
+        let encoded = encode(data);
+
+        for id in IdIter::new(&encoded) {
+            let val = self.internal.entry(id).or_insert(0);
+            *val = val.saturating_add(multiplier);
+        }
+    }
+
+    pub fn scores(&self, data: &[u8]) -> impl Iterator<Item = u32> {
+        let encoded = encode(data);
+
+        IdIter::new(&encoded)
+            .map(|id| self.internal.get(&id).cloned().unwrap_or(0))
+            .collect::<Vec<_>>()
+            .into_iter()
+    }
+
+    pub fn save(&self, path: &Path) -> Result<()> {
+        debug!("Saving index to '{}'...", path.display());
+        let file = atomicwrites::AtomicFile::new(path, atomicwrites::AllowOverwrite);
+        file.write(|f| bincode::serialize_into(f, self))?;
+        debug!("Index saved.");
+        Ok(())
+    }
+
+    pub fn load_or_create(path: &Path) -> Result<Index> {
+        let index;
+
+        if path.exists() {
+            info!("Loading index...");
+            index = bincode::deserialize_from(fs::File::open(path)?)?;
+        } else {
+            info!("Initializing new index...");
+            index = Index::default();
+        };
+
+        info!("Index ready ({} keys).", index.internal.len());
+
+        Ok(index)
+    }
+}
+
+pub fn encode(data: &[u8]) -> Vec<u8> {
+    data.iter().map(|&b| table::ASCII_ID_MAP[b as usize]).filter(|&b| b != 0xFF).collect()
+}
+
+pub fn decode(data: &[u8]) -> Vec<u8> {
+    data.iter().map(|&b| table::ID_ASCII_MAP[b as usize]).collect()
+}
+
+struct IdIter<'a> {
+    windows: slice::Windows<'a, u8>,
+}
+
+impl<'a> IdIter<'a> {
+    fn new(encoded_data: &'a [u8]) -> IdIter<'a> {
+        IdIter { windows: encoded_data.windows(5) }
+    }
+}
+
+impl<'a> Iterator for IdIter<'a> {
+    type Item = u32;
+
+    #[cfg_attr(feature = "cargo-clippy", allow(clippy_complexity))]
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.windows.next().map(|w| 0u32
+            + w[0] as u32
+            + w[1] as u32 * 64
+            + w[2] as u32 * 64 * 64
+            + w[3] as u32 * 64 * 64 * 64
+            + w[4] as u32 * 64 * 64 * 64 * 64)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 #![deny(unused_must_use)]
 
+extern crate atomicwrites;
+extern crate bincode;
 #[macro_use]
 extern crate failure;
+extern crate fnv;
 #[macro_use]
 extern crate hyper;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate log;
 extern crate reqwest;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-pub static USER_AGENT: &str = concat!("rust-ops/rust-log-analyzer ", env!("CARGO_PKG_VERSION"));
-
+pub mod index;
 pub mod travis;
+
+pub use index::Index;
+
+pub static USER_AGENT: &str = concat!("rust-ops/rust-log-analyzer ", env!("CARGO_PKG_VERSION"));
 
 pub type Result<T> = std::result::Result<T, failure::Error>;
